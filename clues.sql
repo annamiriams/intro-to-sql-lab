0d8c50e8ea0fcd9793c8e160a3f74534aa9cf271
annams <annaextrasteckel@gmail.com>
2025-05-13 16:09:35 -0700

    clues 1 and 2 complete

--- clues.sql
+++ clues.sql
@@ -2,26 +2,52 @@
  
 -- Write SQL query here
 
+SELECT name, population FROM countries WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
+
+-- returns:
+    --              name              | population
+    -- -------------------------------+------------
+    --  Holy See (Vatican City State) |       1000
+
+------------------------------------------------------------------------------------------------------
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
 
+-- started with this:
+    -- SELECT code FROM countries WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
+    -- SELECT language FROM countrylanguages WHERE countrycode = 'VAT';
+
+-- refactored so that it could be run without needing the information gathered from the first line above ('VAT'):
+    -- SELECT countrylanguages.language FROM countrylanguages JOIN countries ON countrylanguages.countrycode = countries.code WHERE countries.name = 'Holy See (Vatican City State)' AND countrylanguages.isofficial = 't';
+
+-- refactored again to rename the tables so they're shorter and the query is less verbose:
+SELECT cl.language FROM countrylanguages cl JOIN countries c ON cl.countrycode = c.code WHERE c.name = 'Holy See (Vatican City State)' AND cl.isofficial = 't';
+
+-- returns: 
+    -- language
+    -- ----------
+    -- Italian
+------------------------------------------------------------------------------------------------------
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
 
+------------------------------------------------------------------------------------------------------
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
 
+------------------------------------------------------------------------------------------------------
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
 
+------------------------------------------------------------------------------------------------------
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
@@ -29,6 +55,7 @@
 
 -- Write SQL query here
 
+------------------------------------------------------------------------------------------------------
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
